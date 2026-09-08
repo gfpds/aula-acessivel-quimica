72916c6a950f9e454e07f6778524920619085ae1
gfpds <gfragasilva@gmail.com>
2026-09-08 05:03:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,56 @@
-const CACHE = 'aula-acessivel-v4';
-const FILES = ['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon.svg'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).catch(()=>{})); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))); });
+const CACHE = "aula-acessivel-v5";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./smart.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./assets/icon.svg"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(FILES))
+      .catch(() => {})
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches
+          .open(CACHE)
+          .then(cache => cache.put(request, copy));
+
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
